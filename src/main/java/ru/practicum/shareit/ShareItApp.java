69a242a59d8985b9c67969dfb57b023cb76b4119
somikaaaA <s0mik.a@yandex.ru>
2025-01-07 20:56:27 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,11 +2,10 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //надо будет удалить DataSourceAutoConfiguration, когда будет подключена база
 //иначе не проходят тесты
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 
 public class ShareItApp {
 
