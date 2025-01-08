89c3261c80cbc2b15e45564115b38f350561308b
somikaaaA <s0mik.a@yandex.ru>
2025-01-08 19:49:39 +0300

    доработан ErrorHandler

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.shareit.exception.BadRequestException;
+import ru.practicum.shareit.exception.*;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -55,5 +55,40 @@ public class ErrorHandler {
     public ErrorResponse handleMissingField(JsonMappingException e) { //исключение для отсутствия поля в JSOn
         log.error("В JSON не указано обязательно поле");
         return new ErrorResponse("В запросе отсутствует обязательное поле" + e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND) //404
+    public ErrorResponse invalidUserId(InvalidUserIdException e) { //исключение для некорректного номер id user
+        log.error("Неверно указан userId");
+        return new ErrorResponse("В запросе неверно указан userId " + e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //500
+    public ErrorResponse invalidEmail(InvalidEmailException e) { //исключение для некорректного email
+        log.error("Неверно указан email");
+        return new ErrorResponse("В запросе неверно указан email. " + e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND) //404
+    public ErrorResponse invalidItemId(InvalidItemIdException e) { //исключение для некорректного номер id item
+        log.error("Неверно указан itemId");
+        return new ErrorResponse("В запросе неверно указан itemId" + e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND) //404
+    public ErrorResponse invalidBookingId(InvalidBookingIdException e) { //исключение для некорректного номер id booking
+        log.error("Неверно указан bookingId");
+        return new ErrorResponse("В запросе неверно указан bookingId" + e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //500
+    public ErrorResponse invalidBookingParameter(InvalidParameterForBooking e) { //исключение для некорректного параметра бронирования
+        log.error("Неверно указан параметр booking " + e.getMessage());
+        return new ErrorResponse("В запросе неверно указан параметр " + e.getMessage());
     }
 }
