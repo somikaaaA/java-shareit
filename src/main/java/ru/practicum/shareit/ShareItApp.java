d30debf853cae16c53093b31a1360164418b3510
somikaaaA <s0mik.a@yandex.ru>
2025-01-09 15:55:10 +0300

    добавлены файлы application.properties и compose.yaml доработана БД

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //надо будет удалить DataSourceAutoConfiguration, когда будет подключена база
 //иначе не проходят тесты
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ShareItApp {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
 	}
-
 }
