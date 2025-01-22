1e7842f23b15dd6f89c97195c315e1ae04b6969a
somikaaaA <s0mik.a@yandex.ru>
2025-01-22 17:11:28 +0300

    checkstyle

--- server/src/main/java/ru/practicum/shareit/request/service/RequestService.java
+++ server/src/main/java/ru/practicum/shareit/request/service/RequestService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface RequestService {
     ItemRequestDto createRequest(ItemRequestDto itemRequestDto, Long userId);
+
     ItemRequest getRequestById(Long id);
+
     List<ItemRequestDto> getRequests(Long id);
+
     List<ItemRequestDto> getAllRequests();
+
     ItemRequestDto getRequest(Long requestId);
 }
